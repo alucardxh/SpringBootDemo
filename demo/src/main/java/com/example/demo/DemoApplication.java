7696f2ab3c42ce007ec6dda747c0e8203f40b1f2
alucardxh <alucardxh@163.com>
2018-01-30 10:00:09 +0800

    初始化项目

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,5 +11,6 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		//System.setProperty("spring.devtools.restart.enabled", "false");
 		SpringApplication.run(DemoApplication.class, args);
+		//哈哈
 	}
 }
