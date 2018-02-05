a11bc9170e4ca60a9c86d45b347a11d452e55158
alucardxh <alucardxh@163.com>
2018-02-05 12:26:14 +0800

    优化jwtFilter代码

--- demo/src/main/java/com/example/demo/common/shiro/JWTFilter.java
+++ demo/src/main/java/com/example/demo/common/shiro/JWTFilter.java
@@ -4,7 +4,6 @@ import org.apache.shiro.web.filter.authc.BasicHttpAuthenticationFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.ServletRequest;
@@ -13,6 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * 需要定义Authorization为header的key
+ */
 public class JWTFilter extends BasicHttpAuthenticationFilter {
 
     private Logger LOGGER = LoggerFactory.getLogger(this.getClass());
@@ -23,8 +25,7 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
      */
     @Override
     protected boolean isLoginAttempt(ServletRequest request, ServletResponse response) {
-        HttpServletRequest req = (HttpServletRequest) request;
-        String authorization = req.getHeader("Authorization");
+        String authorization = getAuthzHeader(request);
         return authorization != null;
     }
 
@@ -33,9 +34,7 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
      */
     @Override
     protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        String authorization = httpServletRequest.getHeader("Authorization");
-
+        String authorization = getAuthzHeader(request);
         JWTToken token = new JWTToken(authorization);
         // 提交给realm进行登入，如果错误他会抛出异常并被捕获
         getSubject(request, response).login(token);
@@ -57,11 +56,6 @@ public class JWTFilter extends BasicHttpAuthenticationFilter {
         if (isLoginAttempt(request, response)) {
             try {
                 executeLogin(request, response);
-                //对参数进行解密
-                HttpServletRequest httpServletRequest = (HttpServletRequest)request;
-                String encryptContentStr = httpServletRequest.getParameter("content");
-
-
             } catch (Exception e) {
                 response401(request, response);
             }
